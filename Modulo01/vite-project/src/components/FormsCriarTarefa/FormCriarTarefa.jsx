232b8fada8d2fec8d1635a13eec0f7ca59b434f5
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-27 15:37:59 -0300

    Novo componente criado com sucesso

--- Modulo01/vite-project/src/components/FormsCriarTarefa/FormCriarTarefa.jsx
+++ Modulo01/vite-project/src/components/FormsCriarTarefa/FormCriarTarefa.jsx
@@ -1,7 +1,9 @@
+import { CampoTexto } from "../../components";
+
 const FormCriarTarefa = () => {
   return (
     <form>
-      
+      <CampoTexto />
     </form>
   );
 };
--- Modulo01/vite-project/src/components/index.js
+++ Modulo01/vite-project/src/components/index.js
@@ -1,4 +1,5 @@
 export * from './Cabecalho';
+export * from './CampoTexto';
 export * from  './Conteudo';
 export * from  './Rodape';
 export * from './FormsCriarTarefa';
