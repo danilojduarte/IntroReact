e748e790831a7fd4e77f22f9783322ff9808383d
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-27 15:50:03 -0300

    Criando tipos possíveis de botões

---
+++ Modulo01/vite-project/src/components/Botao/Botao.jsx
@@ -0,0 +1,13 @@
+
+
+const Botao = (props) => {
+  const { texto, tipo = "primario", ...outrasProps } = props;
+  return (
+    <button tipo={ tipo } {...outrasProps} >
+      { texto }
+    </button>
+  );
+};
+
+
+export { Botao };
