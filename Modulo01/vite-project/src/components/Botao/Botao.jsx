64c1bb257ec47a75ef6bc10eb8b19989ae2e6281
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-28 15:08:14 -0300

    Botao Finalizado

--- Modulo01/vite-project/src/components/Botao/Botao.jsx
+++ Modulo01/vite-project/src/components/Botao/Botao.jsx
@@ -1,7 +1,9 @@
+import { TIPO_BOTAO } from './constants';
+
 import style from './Botao.module.css';
 
 const Botao = (props) => {
-  const { texto, tipo = "primario", ...outrasProps } = props;
+  const { texto, tipo = TIPO_BOTAO.PRIMARIO, ...outrasProps } = props;
   return (
     <button className={style.Botao} tipo={ tipo } {...outrasProps} >
       { texto }
