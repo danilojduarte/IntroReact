baeaf4123dfac035d88c49e5800271a8bda0b245
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-06-11 15:08:28 -0300

    Aplicando estilização dos componentes

--- Modulo01/vite-project/src/Router.jsx
+++ Modulo01/vite-project/src/Router.jsx
@@ -1,12 +1,14 @@
-import { Routes } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import { Inicial, SobreNos } from './pages';
+import { Inicial, SobreNos } from "./pages";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<Inicial/>} />
-      <Route path='/sobre-nos' element={<SobreNos/>} />
+      <Routes path="/">
+        <Route path="/" element={<Inicial />} />
+        <Route path="/sobre-nos" element={<SobreNos />} />
+      </Routes>
     </Routes>
   );
 };
